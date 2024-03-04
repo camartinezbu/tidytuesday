410cf06b35e652a043a7136e68c74abaf909589b
Camilo Martínez Burgos <camartinezbu.contacto@gmail.com>
2024-03-04 17:23:16 -0500

    fix: Update script for 2024-9 plot

--- 2024/2024-week9/plot_2024w9.R
+++ 2024/2024-week9/plot_2024w9.R
@@ -37,25 +37,33 @@ top_tf_idf_per_century <- event_tf_idf |>
   mutate(century_labels_es = case_when(
     events_count == 1 ~ paste0("Siglo ", as.roman(century), ": ", events_count, " evento"),
     events_count > 1 ~ paste0("Siglo ", as.roman(century), ": ", events_count, " eventos"))
+  ) |> 
+  mutate(century_labels_en = case_when(
+    events_count == 21 ~ paste0(century, "st century: ", events_count, " events"),
+    events_count != 21 & events_count == 1 ~ paste0(century, "th century: ", events_count, " event"),
+    events_count != 21 & events_count != 1 ~ paste0(century, "th century: ", events_count, " events"))
   )
 
 century_labels <- top_tf_idf_per_century |> distinct(century_labels_es) |> pull(century_labels_es)
-  
+cent_labels_en <- top_tf_idf_per_century |> distinct(century_labels_en) |> pull(century_labels_en)  
+
 top_tf_idf_per_century_final <- top_tf_idf_per_century |> 
   ungroup() |> 
-  mutate(century_es = factor(century, levels = c(16:21), labels = century_labels))
+  mutate(century_es = factor(century, levels = c(16:21), labels = century_labels)) |> 
+  mutate(century_en = factor(century, levels = c(16:21), labels = cent_labels_en))
 
 
 # Create plot
-ggplot(top_tf_idf_per_century_final, aes(x = 1, y = y, label = word, color = century_es)) +
+ggplot(top_tf_idf_per_century_final, aes(x = 1, y = y, label = word, color = century_en)) +
   geom_text(fontface = "bold") +
   labs(
-    title = "Palabras con mayor puntaje Tf-Idf\npara los eventos ocurridos el\n29 de febrero según Wikipedia",
+    # title = "Palabras con mayor puntaje Tf-Idf\npara los eventos ocurridos el\n29 de febrero según Wikipedia",
+    title = "Words with the highest Tf-Idf score\nfor the events occurring on the\n29th of february according to Wikipedia",
     caption = social_caption("Wikipedia", text_color = "white", 
-                             highlight_color = "grey70")
+                             highlight_color = "grey70", language = "en")
   ) +
   scale_y_reverse() +
-  facet_wrap(~century_es, ncol = 3) +
+  facet_wrap(~century_en, ncol = 3) +
   theme(
     text = element_text(family = "Roboto"),
     plot.title = element_text(size = 20, face = "bold", color = "white",
@@ -75,7 +83,7 @@ ggplot(top_tf_idf_per_century_final, aes(x = 1, y = y, label = word, color = cen
   )
 
 # Export plot
-ggsave("2024/2024-week9/plots/plot_2024w9.png",
+ggsave("2024/2024-week9/plots/plot_2024w9_en.png",
     width = 2000,
     height = 1800,
     units = "px")
